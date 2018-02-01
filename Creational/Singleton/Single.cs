c4d88faf92b30d94d99d6cbfae98e040bc6b4bde
vivek mishra <vivekmishra.sft@gmail.com>
2018-02-01 12:26:38 +0530

    Added project structure.

---
+++ Creational/Singleton/Single.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Single
+    {
+        // Static members are 'eagerly initialized', that is, 
+        // immediately when class is loaded for the first time.
+        // .NET guarantees thread safety for static initialization
+        private static readonly Single _instance = new Single();
+
+        public static Single GetInstance()
+        {
+            return _instance;
+        }
+    }
+}
